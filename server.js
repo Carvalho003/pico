086ab08bc33d48616a030e38a0cda2bce6fbaaee
Hugo Carvalho <hugo.fcarvalho@sptech.school>
2024-10-28 21:51:48 -0300

    adicionar e servir o acesso a pasta node_modules no servidor

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const path = require('path');
 const app = express();
 
 const indexRoutes = require('./routes/index');
+const exp = require('constants');
 // Serve os arquivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
 
 // Rota para a página inicial (index.html)
 app.use('/', indexRoutes);
+
+
 
 
 // Inicia o servidor na porta 3000
